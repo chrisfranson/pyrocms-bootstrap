64534de557a5daef2b1b0190b47741b436fdaf66
Chris Franson <chris.franson@gmail.com>
2012-07-10 14:42:38 +1000

    Update the theme with info for PyroCMS

--- addons/shared_addons/themes/transform/theme.php
+++ addons/shared_addons/themes/transform/theme.php
@@ -1,13 +1,13 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Theme_transformwi extends Theme {
+class Theme_transform extends Theme {
 
-    public $name 				= 'Transform WI';
+    public $name 				= 'Transform';
     public $author 				= 'Transform WI';
     public $author_website 		= '';
-    public $website 			= '';
-    public $description 		= 'Transform WI theme based on Twitter Bootstrap2';
-    public $version 			= '0.8';
+    public $website 			= 'transformwi.com';
+    public $description 		= 'Based on Twitter Bootstrap and jQuery UI';
+    public $version 			= '0.2';
 
 }
 
