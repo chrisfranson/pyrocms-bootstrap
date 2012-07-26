1758d737e97d2ee3cea8d60b5b40d7bd4d98f363
Chris Franson <chris.franson@gmail.com>
2012-07-26 23:03:19 +1000

    Add some options to the theme

--- addons/shared_addons/themes/transform/theme.php
+++ addons/shared_addons/themes/transform/theme.php
@@ -16,7 +16,35 @@ class Theme_transform extends Theme {
 										'type'          => 'select',
 										'options'       => 'yes=Yes|no=No',
 										'is_required'   => TRUE
-									));
+									),
+
+									'brand_font' => array(
+										'title'         => 'Brand Font',
+										'description'   => 'The font for the site name in the header. Must be on Google Web Fonts.',
+										'default'       => '',
+										'type'          => 'text',
+										'options'       => '',
+										'is_required'   => FALSE
+								   	),
+
+									'header_color' => array(
+										'title'         => 'Header Color',
+										'description'   => 'The background color for the site header.',
+										'default'       => '#000',
+										'type'          => 'text',
+										'options'       => '',
+										'is_required'   => FALSE
+								   	),
+
+									'sitewide_css' => array(
+										'title'         => 'Site-wide CSS',
+										'description'   => 'Custom CSS that will be applied to every page on the site.',
+										'default'       => '',
+										'type'          => 'textarea',
+										'options'       => '',
+										'is_required'   => FALSE
+								   	)
+								);
 
 }
 
